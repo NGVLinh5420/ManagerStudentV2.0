33d2f62e6ea2219f4ddc021d571a7294e850a83e
NGVLinh5420 <ngvlinh101@gmail.com>
2023-12-03 14:14:17 +0700

    Coming Soon: Tìm Kiếm

---
+++ app/src/main/java/com/example/managerstudent/Linh/L_TimKiem_Activity.java
@@ -0,0 +1,16 @@
+package com.example.managerstudent.Linh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.managerstudent.R;
+
+public class L_TimKiem_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.l_gui_tim_kiem);
+    }
+}
